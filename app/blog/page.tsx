5f502fe40200a0b59133540e9b99e74867e0500a
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-02-24 20:03:19 -0300

    feat: Add blog index page

---
+++ app/blog/page.tsx
@@ -0,0 +1,25 @@
+import BlogAside from "@/components/ui/blog/Aside"
+import PostCard from "@/components/ui/post/PostCard"
+import { getPosts, POSTS_KEYWORDS } from "@/lib/posts/mdx"
+import React from "react"
+
+const BlogIndex = () => {
+	const posts = getPosts()
+	return (
+		<div className="px-5 text-[#333] flex gap-10 flex-col md:flex-row">
+			<main className="flex-1">
+				<h1 className="font-semibold text-xl mt-3 mb-4">
+					Últimas publicaciones
+				</h1>
+				<div className="flex flex-col gap-4">
+					{posts.map((post) => (
+						<PostCard {...post} key={post.slug} />
+					))}
+				</div>
+			</main>
+			<BlogAside keywords={POSTS_KEYWORDS} />
+		</div>
+	)
+}
+
+export default BlogIndex
