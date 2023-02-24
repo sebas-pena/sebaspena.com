9a7502aeb451fb2ce1dfbf5bd023c17708a53de0
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-02-24 19:59:10 -0300

    feat: Add Header and Footer to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
-import './globals.css'
+import Footer from "@/components/layout/Footer"
+import Header from "@/components/layout/Header"
+import "../styles/globals.css"
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<head />
+			<body className="font-sans flex flex-col min-w-screen min-h-screen">
+				<Header />
+				<div className="flex-1 flex flex-col w-full max-w-5xl m-auto">
+					{children}
+				</div>
+				<Footer />
+			</body>
+		</html>
+	)
 }
