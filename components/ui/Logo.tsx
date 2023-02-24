77a69649c2a2717eb27728e6c5f664acfd532de7
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-02-24 19:49:01 -0300

    feat: Add Logo component

---
+++ components/ui/Logo.tsx
@@ -0,0 +1,25 @@
+import { LogoType } from "@/types/post"
+import Image from "next/image"
+import React from "react"
+
+const Logo = ({
+	type,
+	className = "",
+}: {
+	className?: string
+	type: LogoType
+}) => {
+	return (
+		<picture>
+			<Image
+				src={`/images/logos/${type}.webp`}
+				width={70}
+				height={70}
+				className={`rounded-lg ${className}`}
+				alt={`${type} logo`}
+			/>
+		</picture>
+	)
+}
+
+export default Logo
