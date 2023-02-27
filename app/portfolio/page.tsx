48519d48986c9054234204ab33674ee5f1404394
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-02-27 02:54:38 -0300

    feat: Display list of projects

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,7 +1,21 @@
+import ProjectCard from "@/components/ui/ProjectCard"
+import { getProjects } from "@/lib/projects/projects"
 import React from "react"
 
-const ProjectListPage = () => {
-	return <div>ProjectListPage</div>
+const ProjectListPage = async () => {
+	const projects = await getProjects()
+	return (
+		<main className="flex-1 px-4">
+			<h1 className="font-semibold text-2xl mt-3 mb-4">Proyectos</h1>
+			<ul className="grid grid-cols-1 min-[600px]:grid-cols-2  md:grid-cols-1 gap-4 p-4">
+				{projects.map((project) => (
+					<li key={project.title}>
+						<ProjectCard {...project} />
+					</li>
+				))}
+			</ul>
+		</main>
+	)
 }
 
 export default ProjectListPage
