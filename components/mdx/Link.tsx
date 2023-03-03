74629ea5dced0c41d559cc97369b7e4bb3e109a1
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-03 02:57:52 -0300

    fix: change Types

--- components/mdx/Link.tsx
+++ components/mdx/Link.tsx
@@ -1,16 +1,21 @@
-import Link from "next/link"
 import React from "react"
 
-const A = ({ href, children }: { href: string; children: string }) => {
+const A = ({
+	href,
+	children,
+}: {
+	href?: string
+	children?: React.ReactNode
+}) => {
 	return (
-		<Link
+		<a
 			href={href}
 			className="text-blue-600 underline"
 			target="_blank"
-			rel="noopener nofollow"
+			rel="noopener nofollow noreferrer"
 		>
 			{children}
-		</Link>
+		</a>
 	)
 }
 
