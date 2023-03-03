1a9ad7afaa78702bdfe8e4da4c96a1f709a50ea9
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-02 21:46:23 -0300

    fix: Display two columns when there are two or more projects

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -7,7 +7,11 @@ const ProjectListPage = async () => {
 	return (
 		<main className="flex-1 px-4">
 			<h1 className="font-semibold text-2xl mt-3 mb-4">Proyectos</h1>
-			<ul className="grid grid-cols-1 min-[600px]:grid-cols-2  md:grid-cols-1 gap-4 p-4">
+			<ul
+				className={`grid grid-cols-1 ${
+					projects.length > 1 ? "min-[600px]:grid-cols-2" : ""
+				}  md:grid-cols-1  gap-4 p-4`}
+			>
 				{projects.map((project) => (
 					<li key={project.title}>
 						<ProjectCard {...project} />
