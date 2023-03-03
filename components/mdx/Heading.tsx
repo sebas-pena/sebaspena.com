d1951cd419a83cf707ee8684e76dc0e52745a5ae
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-03 02:47:37 -0300

    feat: Increase top margin

--- components/mdx/Heading.tsx
+++ components/mdx/Heading.tsx
@@ -4,42 +4,42 @@ type HeadingProps = {
 
 const H1 = ({ children }: HeadingProps) => {
 	return (
-		<h1 className="mt-6 mb-5 text-primary-700 font-semibold text-4xl">
+		<h1 className="mt-9 mb-5 text-primary-700 font-semibold text-4xl">
 			{children}
 		</h1>
 	)
 }
 const H2 = ({ children }: HeadingProps) => {
 	return (
-		<h2 className="mt-6 mb-5 text-primary-700 font-semibold text-3xl">
+		<h2 className="mt-9 mb-5 text-primary-700 font-semibold text-3xl">
 			{children}
 		</h2>
 	)
 }
 const H3 = ({ children }: HeadingProps) => {
 	return (
-		<h3 className="mt-6 mb-5 text-primary-700 font-semibold text-2xl">
+		<h3 className="mt-9 mb-5 text-primary-800 font-semibold text-2xl">
 			{children}
 		</h3>
 	)
 }
 const H4 = ({ children }: HeadingProps) => {
 	return (
-		<h4 className="mt-6 mb-5 text-primary-700 font-semibold text-xl">
+		<h4 className="mt-9 mb-5 text-primary-800 font-semibold text-xl">
 			{children}
 		</h4>
 	)
 }
 const H5 = ({ children }: HeadingProps) => {
 	return (
-		<h5 className="mt-6 mb-5 text-primary-700 font-semibold text-lg">
+		<h5 className="mt-9 mb-5 text-primary-800 font-semibold text-lg">
 			{children}
 		</h5>
 	)
 }
 const H6 = ({ children }: HeadingProps) => {
 	return (
-		<h6 className="mt-6 mb-5 text-primary-700 font-semibold text-lg">
+		<h6 className="mt-9 mb-5 text-primary-800 font-semibold text-lg">
 			{children}
 		</h6>
 	)
