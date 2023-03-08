f56f384c31e213dd8a768a8b1666a3a35adf85fb
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-08 15:08:27 -0300

    fix: Add margin bottom

--- components/mdx/Lists.tsx
+++ components/mdx/Lists.tsx
@@ -1,9 +1,9 @@
 import React from "react"
 
 export const Ol = ({ children }: { children?: React.ReactNode }) => {
-	return <ol className="list-decimal pl-4">{children}</ol>
+	return <ol className="list-decimal pl-4 mb-6">{children}</ol>
 }
 
 export const Ul = ({ children }: { children?: React.ReactNode }) => {
-	return <ul className="list-disc">{children}</ul>
+	return <ul className="list-disc pl-4 mb-6">{children}</ul>
 }
